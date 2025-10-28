8e0b912673200eb422587e94646ba65b164aa38f
tado <tmandangu@africancentury.co.zw>
2025-10-28 10:20:09 +0200

    Rate limiting project setup

---
+++ src/main/java/com/example/rate_limiting_demo/RateLimitingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rate_limiting_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimitingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rate_limiting_demo/RateLimitingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rate_limiting_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
